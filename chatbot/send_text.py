cd35dada47d15a92ca8d8baca4731cdb7f9fffdc
Russell <rratcliffe57@gmail.com>
2017-03-03 21:02:40 -0500

    added basic chatbot implementation

---
+++ chatbot/send_text.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os.path
+import sys
+
+try:
+    import apiai
+except ImportError:
+    sys.path.append(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+    )
+    import apiai
+
+CLIENT_ACCESS_TOKEN = 'c066ae29c94d42ac87e84edfec16e046'
+
+
+def main():
+    ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+
+    request = ai.text_request()
+
+    request.lang = 'de'  # optional, default value equal 'en'
+
+    request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
+
+    request.query = "1"
+
+    response = request.getresponse()
+
+    print (response.read())
+
+
+if __name__ == '__main__':
+    main()
