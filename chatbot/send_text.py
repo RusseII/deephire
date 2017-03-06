5d40349722b10b922fb0cf5287222e4e74b76e5e
Russell <rratcliffe57@gmail.com>
2017-03-05 23:01:08 -0500

    hooked up bot

--- chatbot/send_text.py
+++ chatbot/send_text.py
@@ -11,24 +11,27 @@ except ImportError:
     )
     import apiai
 
-CLIENT_ACCESS_TOKEN = 'c066ae29c94d42ac87e84edfec16e046'
-
+CLIENT_ACCESS_TOKEN = 'c066ae29c94d42ac87e84edfec16e046' 
 
 def main():
     ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
 
     request = ai.text_request()
 
-    request.lang = 'de'  # optional, default value equal 'en'
+   # request.lang = 'de'  # optional, default value equal 'en'
 
     request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
 
-    request.query = "1"
+    request.query = "test"
 
     response = request.getresponse()
 
     print (response.read())
+    
+    request.query = "two"
 
+    #response = request.getresponse()
 
+    print (response.read())
 if __name__ == '__main__':
     main()
--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
 import os
+from bot import chat 
 import subprocess
 from flask import Flask,flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
@@ -17,13 +18,19 @@ thread = None
 
 @app.route('/', methods=['GET', 'POST'] )
 def upload_file():
-    return render_template("index.html",async_mode=socketio.async_mode)
+    return render_template("index.html", async_mode=socketio.async_mode)
 
-@socketio.on('loaded')
-def page_load():
-    newvisitor()
-    newprint()
+#@socketio.on('loaded')
+#def page_load():
+#    newvisitor()
+#    newprint()
+#
 
+@socketio.on('message_sent')
+def get_message(msg):
+    socketio.emit("message_recieved", chat(msg))
+    
+	 
 def newvisitor():
     visitors = db().query("SELECT visits FROM stats")[0][0]
     socketio.emit('visitor_update',
