4f21302ad2734ed0383b5c2e74dae8004cb73eac
NickCrawford <ncrawfo7@kent.edu>
2017-03-06 11:52:26 -0500

    Adds some ui improvements

--- code/static/js/main.js
+++ code/static/js/main.js
@@ -7,11 +7,32 @@ var socket = io.connect(location.protocol + '//' + document.domain + ':' + locat
 // The callback function is invoked when a connection with the
 // server is established.
 
+// Bind send message to text box
+
+document.getElementById("human-textbox")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        sendMessage();
+    }
+});
+
 function sendMessage() {
   var msg = document.getElementById('human-textbox').value;
   console.log(msg);
 
+  // Exit if input is empty
+  if (!msg.trim() || msg.trim() == '') return;
+
+  // Populate message in chat container
   populateHumanMessage(msg);
+
+  // Clear input value
+  document.getElementById('human-textbox').value = '';
+
+  // Scroll to bottom of chat
+  var chatCtn = document.getElementById("chat-container");
+  chatCtn.scrollTop = chatCtn.scrollHeight;
 
   socket.emit('message_sent', msg);
 }
@@ -21,7 +42,7 @@ function populateAIMessage(msg) {
     element.innerHTML = msg;
     element.className = 'chat-bubble ai-chat-bubble';
 
-    document.getElementById("ai-chat-container").appendChild(element);
+    document.getElementById("chat-container").appendChild(element);
 }
 
 function populateHumanMessage(msg) {
@@ -29,7 +50,7 @@ function populateHumanMessage(msg) {
     element.innerHTML = msg;
     element.className = 'chat-bubble human-chat-bubble';
     //"<div class='chat-bubble ai-chat-bubble'>"+ msg +"</div>");
-    document.getElementById("human-chat-container").appendChild(element);//.appendChild("<div class='chat-bubble ai-chat-bubble'>"+ msg +"</div>")
+    document.getElementById("chat-container").appendChild(element);//.appendChild("<div class='chat-bubble ai-chat-bubble'>"+ msg +"</div>")
 }
 
 socket.on('message_recieved', function(msg) {
